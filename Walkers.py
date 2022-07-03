5d9a91d23a7664823c8c9b9cb1c1bfedce12a4e6
payelbera <60781680+payelbera@users.noreply.github.com>
2022-07-03 16:04:02 +0530

    Update Walkers.py

--- Walkers.py
+++ Walkers.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 
 # Create our body classifier
-body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
 
 # Initiate video capture for video file
-cap = cv2.VideoCapture('walking.avi')
+
 
 # Loop once video is successfully loaded
 while True:
     
     # Read first frame
-    ret, frame = cap.read()
-    #frame = cv2.resize(frame, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
+    
+   
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Pass frame to our body classifier
-    bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
+    
     
     # Extract bounding boxes for any bodies identified
     for (x,y,w,h) in bodies:
